8d369a61cf3a5ecf8933c57a8701b267574056fb
Rasul Iskandarov <73297277+rasul-isk@users.noreply.github.com>
2023-03-27 10:16:56 +0300

    started backend

---
+++ server/loan/src/main/java/com/loanmatcher/loan/LoanMatcherApplication.java
@@ -0,0 +1,13 @@
+package com.loanmatcher.loan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMatcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMatcherApplication.class, args);
+	}
+
+}
